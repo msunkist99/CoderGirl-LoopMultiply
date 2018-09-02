8af28e3de036bb02cc65ad8ac5ff82980215ec36
Scott Kuhl <scott@kuhl.ws>
2018-09-02 01:02:12 -0500

    Initial project.

--- LoopMultiply/Program.cs
+++ LoopMultiply/Program.cs
@@ -2,11 +2,17 @@
 
 namespace LoopMultiply
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a number: ");
+
+            // TODO: Loop from 2 to 20 and multiply the loop value by the number the user entered and output the value each time. 
+
+            // TODO: When the loop is finished, output the sum of all previous values.
+
+            Console.ReadLine();
         }
     }
 }
