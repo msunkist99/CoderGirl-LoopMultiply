df136bd9646af29e5348944cd36e5bf8a2cab2f6
msunkist99 <msunkist99@gmail.com>
2019-01-28 18:40:51 -0600

    2019/10/29 initial code

--- LoopMultiply/Program.cs
+++ LoopMultiply/Program.cs
@@ -8,10 +8,18 @@ namespace LoopMultiply
         {
             Console.Write("Enter a number: ");
 
+            int number = int.Parse(Console.ReadLine());
+            int total = 0;
+
             // TODO: Loop from 2 to 20 and multiply the loop value by the number the user entered and output the value each time. 
+            for (int i = 2; i < 21; i++)
+            {
+                total += (number * i);
+                Console.WriteLine((number * i) + " - " + total);
+            }
 
             // TODO: When the loop is finished, output the sum of all previous values.
-
+            Console.WriteLine(total);
             Console.ReadLine();
         }
     }
